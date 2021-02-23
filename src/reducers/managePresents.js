8971c970cf676dda98ae59875b9e07a698fbf321
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-02-23 23:25:40 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++
@@ -1,8 +0,0 @@
-export function managePresents(state = { numberOfPresents: 1 }, action) {
-  switch (action.type) {
-    case "INCREASE":
-      return { numberOfPresents: state.numberOfPresents + 1 };
-    default:
-      return state;
-  }
-}
